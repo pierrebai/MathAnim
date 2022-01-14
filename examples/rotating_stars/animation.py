d34f60f61c478a4a41e2e792ecb6b1b5836634c4
Pierre Baillargeon <pierrebai@hotmail.com>
2022-01-13 22:12:07 -0500

    Draw the star and fix the animation angles. ALso fix teh case where some animations are empty.

--- examples/rotating_stars/animation.py
+++ examples/rotating_stars/animation.py
@@ -123,16 +123,18 @@ class animation(anim.animation):
         formed by the dot on the inner circle, forming the star.
         Keep it in the animator to avoid re-recreating on each frame.
         """
-        dot = self.inner_dots_pos[0][0]
-        center = self.inner_centers[0]
-        star_segments = 240 // max(self.skip, 1)
+        outer_angle = 360. * self.skip
+        inner_angle = 360. * self.inner_count
+        center = anim.point(self.inner_centers[0].original_point)
+        dot = anim.relative_point(center, self.inner_dots_pos[0][0].original_point)
         pts = []
-        # for i in range(star_segments * self.skip + 1):
-        #     outer_angle = 360. * self.sides * float(i) / float(star_segments)
-        #     inner_angle = outer_angle / self.inner_circle_ratio
-        #     new_pos = anim.point(anim.trf.rotate_around(dot, center, inner_angle))
-        #     new_pos = anim.point(anim.trf.rotate_around(new_pos, anim.point(0., 0.), outer_angle))
-        #     pts.append(new_pos)
+        star_segment_count = 240
+        for i in range(star_segment_count):
+            angle = outer_angle * float(i) / float(star_segment_count)
+            center.set_point(anim.trf.rotate_around_origin(center.original_point, angle))
+            angle = -inner_angle * float(i) / float(star_segment_count)
+            dot.set_point(anim.trf.rotate_around_origin(dot.original_point, angle))
+            pts.append(anim.point(dot))
         poly = anim.items.create_polygon(pts, anim.items.dark_gray_color)
         self.star = anim.actor("star", "The star that the dots on the inner circle follow.", poly)
         self.add_actors(self.star, scene)
@@ -250,9 +252,12 @@ class animation(anim.animation):
         """
         def prep_anim(scene: anim.scene, animator: anim.animator):
             dots = [self.inner_dots[which_inner][which_dot] for which_dot in range(1, self.dots_count)]
-            for dot in dots:
-                reveal = anim.reveal_item(dot)
-                animator.animate_value(0., 1., self.reveal_duration, reveal)
+            if dots:
+                for dot in dots:
+                    reveal = anim.reveal_item(dot)
+                    animator.animate_value(0., 1., self.reveal_duration, reveal)
+            else:
+                animator.check_all_anims_done()
 
         self.add_shots(anim.shot("Draw the other dots on the inner circle", "", prep_anim))
 
@@ -284,9 +289,12 @@ class animation(anim.animation):
         """
         def prep_anim(scene: anim.scene, animator: anim.animator):
             polys = [self.inter_polygons[which_dot] for which_dot in range(0, self.dots_count)]
-            for poly in polys:
-                reveal = anim.reveal_item(poly)
-                animator.animate_value(0., 1., self.reveal_duration, reveal)
+            if polys:
+                for poly in polys:
+                    reveal = anim.reveal_item(poly)
+                    animator.animate_value(0., 1., self.reveal_duration, reveal)
+            else:
+                animator.check_all_anims_done()
 
         self.add_shots(anim.shot("Draw the inter-circle polygons", "", prep_anim))
 
@@ -294,9 +302,9 @@ class animation(anim.animation):
         """
         Animate all the inner circles and their polygons.
         """
+        outer_angle = 360. * self.skip
+        inner_angle = 360. * self.inner_count
         def prep_anim(scene: anim.scene, animator: anim.animator):
-            outer_angle = 360.
-            inner_angle = outer_angle #/ self.inner_circle_ratio
             for which_inner in range(self.inner_count):
                 center = self.inner_centers[which_inner]
                 rot_center = anim.rotate_point_around(center, anim.point(0., 0.))
@@ -304,6 +312,6 @@ class animation(anim.animation):
                 for which_dot in range(self.dots_count):
                     dot = self.inner_dots_pos[which_inner][which_dot]
                     rot_dot = anim.rotate_point_around(dot, anim.point(0., 0.))
-                    animator.animate_value(inner_angle, 0., 2. * self.animation_speedup, rot_dot)
+                    animator.animate_value(0., -inner_angle, 2. * self.animation_speedup, rot_dot)
 
         self.add_shots(anim.shot("Animate all", "", prep_anim))
